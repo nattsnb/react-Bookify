a30a330c525168cfb9eaca00ab116b5119eb2f34
Natasza ASzatan <natiye.nb@gmai.com>
2024-10-21 15:28:15 +0200

    added git router dom, added browser router. placeholders displays in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-import {createTheme, ThemeProvider} from "@mui/system";
-import {Route, Router, Routes} from "react-router";
+import { createTheme, ThemeProvider } from "@mui/system";
+import { Route, Router, Routes } from "react-router";
 import styles from "./app.module.css";
 
 const bookifyMuiTheme = createTheme({
-    palette: {
-        primary: {
-            main: "#67AA92",
-            contrastText: "#FFFFFF"
-        }
-    }
-})
- 
+  palette: {
+    primary: {
+      main: "#67AA92",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
 
-function App() {
-
-    return (
-        <ThemeProvider theme={bookifyMuiTheme}>
-            <div className={styles.header}>
-                PLACEHOLDER HEADER
-            </div>
-            <Routes>
-                <Route path="/" elelemnt={{
-                }}/>
-                <Route
-                    path="/..."
-                    element={
-                    }
-                />
-            </Routes>
-            <div className={styles.footer}>
-                PLACEHOLDER FOOTER
-            </div>
-        </ThemeProvider>
-    )
+export function App() {
+  return (
+    <ThemeProvider theme={bookifyMuiTheme}>
+      <div className={styles.header}>PLACEHOLDER HEADER</div>
+      <Routes>
+        <Route path="/" />
+        <Route path="/..." />
+      </Routes>
+      <div className={styles.footer}>PLACEHOLDER FOOTER</div>
+    </ThemeProvider>
+  );
 }
