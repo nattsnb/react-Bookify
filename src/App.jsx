971b1174d42d69475a7643a89e5260da618e2ba3
Natasza ASzatan <natiye.nb@gmai.com>
2024-10-24 11:41:11 +0200

    dded folders components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { createTheme, ThemeProvider } from "@mui/system";
 import { Route, Router, Routes } from "react-router";
 import styles from "./app.module.css";
-import { Results } from "./Results/index.jsx";
-import { Venue } from "./Venue/index.jsx";
-import { NavBar } from "./NavBar/index.jsx";
-import { Footer } from "./Footer/index.jsx";
-import { LayOut } from "./LayOut/index.jsx";
+import { Results } from "./pages/Results/index.jsx";
+import { Venue } from "./pages/Venue/index.jsx";
+import { NavBar } from "./components/NavBar/index.jsx";
+import { Footer } from "./components/Footer/index.jsx";
+import { LayOut } from "./components/LayOut/index.jsx";
 
 const bookifyMuiTheme = createTheme({
   palette: {
