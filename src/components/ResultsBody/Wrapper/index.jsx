ff794bcd74d02423d4158554350b6f6c8f8558d6
Natasza ASzatan <natiye.nb@gmai.com>
2024-11-05 18:47:27 +0100

    added div sections to results body wrapper

--- src/components/ResultsBody/Wrapper/index.jsx
+++ src/components/ResultsBody/Wrapper/index.jsx
@@ -4,5 +4,11 @@ import { Icon, Link } from "@mui/material";
 export function Wrapper({ arrayOfSMLinks }) {
   const arrayOfIcons = [];
 
-  return <div className={styles.wrapper}>Results Cards</div>;
+  return <div className={styles.wrapper}>
+    <div className={styles.toolbar}></div>
+    <div className={styles.cardsWrapper}></div>
+    <div className={styles.pagination}>
+      <div className={styles.container}></div>
+    </div>
+  </div>;
 }
